23e4a973b355399600a048d321b76fcfcdeb11c2
lazllb <lazllb@5bcc4e6b-a972-b8c6-4cac-e6b1f1d797d6>
2010-05-08 20:20:07 +0000

    Added the copyright header to all classes.

--- trunk/rt5e/core/src/org/lazaro/rt5e/io/SQLHandler.java
+++ trunk/rt5e/core/src/org/lazaro/rt5e/io/SQLHandler.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2010 Lazaro Brito
+ *
+ * This file is part of RT5E.
+ *
+ * RT5E is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RT5E is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RT5E.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.lazaro.rt5e.io;
 
 import org.lazaro.rt5e.utility.Configuration;
